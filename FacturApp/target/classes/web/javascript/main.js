5a1959d5ad5efdf712b42eaf2a1601d6022900a3
gabrimrt02 <gabrimrt02@gmail.com>
2023-04-04 23:11:44 +0200

    Update Web
    Modificada la lógica de la carga de páginas dentro del frame.

--- FacturApp/target/classes/web/javascript/main.js
+++ FacturApp/target/classes/web/javascript/main.js
@@ -1,20 +1,52 @@
 // TODO Implementar los Scripts interactivos de la página principal
+
+/*
+ * Variables de elementos
+*/
 var documento = $(document);
-var btnPrimera;
+var botones = [];
 var frame;
+
+/*
+ * Variables
+*/
+var lastLoaded = ""; // Almacena el ultimo recurso cargado dentro del frame
+
 documento.ready(inicio);
 
 function inicio() {
-    btnPrimera = $("#boton1");
-    btnSegunda = $("#boton2");
+    botones.push($("#boton1"));
+    botones.push($("#boton2"));
+
     frame = $("#frame");
-    btnPrimera.click(function(e) {
-        console.log("HolaMundo!!");
-        frame.attr("src", "../html/sections/balance.html");
-    });
-    btnSegunda.click(function(e) {
-        console.log("HolaMundo!!");
-        frame.attr("src", "../html/sections/test.html");
+    lastLoaded = frame.attr("src");
+
+    $.each(botones, function (indexInArray, valueOfElement) {
+        botones[indexInArray].click(function (e) {
+            var pageToLoad = "";
+
+            switch (indexInArray) {
+                case 0:
+                    pageToLoad = "../html/sections/balance.html";
+                    break;
+
+                case 1:
+                    pageToLoad = "../html/sections/test.html";
+                    break;
+
+                default:
+                    break;
+            }
+
+            console.log("A cargar: " + pageToLoad)
+
+            if (pageToLoad != lastLoaded) {
+                frame.attr("src", pageToLoad);
+                lastLoaded = frame.attr("src");
+            } else {
+                console.log('Ya se encuentra en la ventana');
+            }
+        });
     });
 
 }
